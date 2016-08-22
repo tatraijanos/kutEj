46f7ab4e3aa11739aea4aab8b7aa174e737855c8
Tátrai János <hunjoy@gmail.com>
2016-08-23 00:47:35 +0200

    link javítás

--- partials/lablec.php
+++ partials/lablec.php
@@ -2,7 +2,7 @@
 		<div>
 			<p><b>Kutatók Éjszakája 2016 - Prím megjelenitő</b></p>
 			<p>Tátrai János Norbert - Szabó Norbert</p>
-			<p>https://github.com/hunjoy/kutEj</p>
+			<p>https://github.com/tatraijanos/kutEj</p>
 		</div>
 	
 	</body>
